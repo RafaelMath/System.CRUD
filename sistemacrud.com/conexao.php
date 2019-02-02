a6d293e001d923210657b7d38d6d67b45043ce66
RafaelMath <rgildo38@gmail.com>
2019-02-02 14:32:35 -0300

    init.systemcrud.com

---
+++ sistemacrud.com/conexao.php
@@ -0,0 +1,9 @@
+<?php
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "sistemacrud.com";
+
+//Cria a conexao com o db
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
